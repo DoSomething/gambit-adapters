3c796b5804df7aeff2698f18831fb1c886ffb5ba
Aaron Schachter <aaronschachter@gmail.com>
2017-03-10 13:33:44 -0500

    List Gambit campaigns

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var Botkit = require('botkit');
+const Botkit = require('botkit');
 
 const controller = Botkit.slackbot();
 const bot = controller.spawn({
   token: process.env.SLACK_API_TOKEN,
 });
+
+const request = require('superagent');
+const gambitBaseUri = 'https://ds-mdata-responder.herokuapp.com/v1/'
 
 bot.startRTM((err, bot, payload) => {
   if (err) {
@@ -11,10 +14,21 @@ bot.startRTM((err, bot, payload) => {
   }
 });
 
-controller.hears('puppet', ['mention', 'ambient'], (bot,message) => {
+controller.hears('puppet', ['mention', 'ambient'], (bot, message) => {
   bot.reply(message, 'Puppet!');
 });
 
 controller.hears('eye', ['mention', 'ambient'], (bot, message) => {
   bot.reply(message, 'Somebody, please... fix my eye.');
+});
+
+controller.hears('keywords', ['mention', 'direct_message'], (bot, message) => {
+  let output = 'Campaigns running on Gambit production:\n';
+  request.get(`${gambitBaseUri}campaigns`)
+    .then((response) => {
+      response.body.data.forEach((campaign) => {
+        output = `${output} - ${campaign.title}\n`
+      })
+      bot.reply(message, output);
+    })
 });
