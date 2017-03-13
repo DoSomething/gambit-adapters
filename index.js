b931df5c690815908d4319446a9274919a6b9764
Aaron Schachter <aaronschachter@gmail.com>
2017-03-13 16:49:34 -0400

    Render campaigns as attachments

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Botkit = require('botkit');
 
 const controller = Botkit.slackbot();
@@ -23,12 +25,41 @@ controller.hears('eye', ['mention', 'ambient'], (bot, message) => {
 });
 
 controller.hears('keywords', ['mention', 'direct_message'], (bot, message) => {
-  let output = 'Campaigns running on Gambit production:\n';
+  console.log('keywords');
+  let output = {
+    username: 'Puppet Sloth',
+    text: 'Campaigns running on Gambit production:',
+    attachments: [],
+  };
+
+  const colors = [ 'FCD116', '23b7fb', '4e2b63' ];
+
   request.get(`${gambitBaseUri}campaigns`)
     .then((response) => {
-      response.body.data.forEach((campaign) => {
-        output = `${output} - ${campaign.title}\n`
-      })
+      response.body.data.forEach((campaign, index) => {
+        const keywords = campaign.keywords.map(entry => entry.keyword);
+
+        const attachment = {
+          color: `#${colors[index % 3]}`,
+          title: `${campaign.title} (ID ${campaign.id})`,
+          title_link: `https://dosomething.org/node/${campaign.id}`,
+          fields: [
+            {
+              title: 'Keywords',
+              value: keywords.join(', '),
+              short: true,
+            },
+            {
+              title: 'Status',
+              value: campaign.status,
+              short: true,
+            },
+          ],
+        }
+        output.attachments.push(attachment);
+      });
+
       bot.reply(message, output);
     })
+    .catch(err => console.log(err));
 });
