412ea9def3a5ca6a79755340ef80e121d817d90e
Aaron Schachter <aaronschachter@gmail.com>
2017-03-13 19:14:58 -0400

    Adds fetchCampaigns(environmentName) to support prod and thor camapigns

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const Promise = require('bluebird');
 const Botkit = require('botkit');
 const request = require('superagent');
 
@@ -10,48 +13,70 @@ slothbot.startRTM((err) => {
   }
 });
 
-controller.hears('puppet', ['mention', 'ambient'], (bot, message) => {
-  bot.reply(message, 'Puppet!');
-});
+/**
+ * Returns Gambit URI for production, or Thor if set as environmentName.
+ */
+function gambitUri(environmentName) {
+  let subdomain = 'ds-mdata-responder';
+  if (environmentName === 'thor') {
+    subdomain = `${subdomain}-staging`;
+  }
+  return `https://${subdomain}.herokuapp.com/v1/`;
+}
 
-controller.hears('eye', ['mention', 'ambient'], (bot, message) => {
-  bot.reply(message, 'Somebody, please... fix my eye.');
-});
-
-controller.hears('keywords', ['mention', 'direct_message'], (bot, message) => {
+/**
+ * Fetches Gambit campaigns on production, or for given environment.
+ */
+function fetchCampaigns(environmentName) {
   const output = {
     username: 'Puppet Sloth',
-    text: 'Campaigns running on Gambit production:',
+    text: `Gambit Campaigns running on *${environmentName.toUpperCase()}*:`,
+    mrkdwn: true,
     attachments: [],
   };
 
   const colors = ['FCD116', '23b7fb', '4e2b63'];
-  const gambitBaseUri = 'https://ds-mdata-responder.herokuapp.com/v1/';
 
-  request.get(`${gambitBaseUri}campaigns`)
-    .then((response) => {
-      response.body.data.forEach((campaign, index) => {
-        const keywords = campaign.keywords.map(entry => entry.keyword);
-        output.attachments.push({
-          color: `#${colors[index % 3]}`,
-          title: `${campaign.title} (ID ${campaign.id})`,
-          title_link: `https://dosomething.org/node/${campaign.id}`,
-          fields: [
-            {
-              title: 'Keywords',
-              value: keywords.join(', '),
-              short: true,
-            },
-            {
-              title: 'Status',
-              value: campaign.status,
-              short: true,
-            },
-          ],
+  return new Promise((resolve, reject) => {
+    const uri = `${gambitUri(environmentName)}campaigns`;
+
+    return request.get(uri)
+      .then((response) => {
+        response.body.data.forEach((campaign, index) => {
+          const keywords = campaign.keywords.map(entry => entry.keyword);
+          output.attachments.push({
+            color: `#${colors[index % 3]}`,
+            title: `${campaign.title} (ID ${campaign.id})`,
+            title_link: `https://dosomething.org/node/${campaign.id}`,
+            fields: [
+              {
+                title: 'Keywords',
+                value: keywords.join(', '),
+                short: true,
+              },
+              {
+                title: 'Status',
+                value: campaign.status,
+                short: true,
+              },
+            ],
+          });
         });
-      });
 
-      bot.reply(message, output);
-    })
+        return resolve(output);
+      })
+    .catch(err => reject(err));
+  });
+}
+
+controller.hears('keywords', ['mention', 'direct_message'], (bot, message) => {
+  fetchCampaigns('production')
+    .then(response => bot.reply(message, response))
+    .catch(err => bot.reply(message, err.message));
+});
+
+controller.hears('thor', ['mention', 'direct_message'], (bot, message) => {
+  fetchCampaigns('thor')
+    .then(response => bot.reply(message, response))
     .catch(err => bot.reply(message, err.message));
 });
