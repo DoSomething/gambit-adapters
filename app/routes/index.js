09f29fc94589111b15a46923f1b5fca3a3284a87
Aaron Schachter <aaronschachter@gmail.com>
2017-07-20 20:22:38 -0700

    Removes twilio

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,6 @@ const slackRoute = require('./slack');
 
 module.exports = function init(app) {
   app.get('/', homeRoute);
-  app.use('/send-message', sendMessageRoute);
+  app.use('/v1/send-message', sendMessageRoute);
   app.use('/slack/receive', slackRoute);
 };
