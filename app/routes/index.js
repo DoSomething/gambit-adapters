b6f4ea61d0c5434b19cfe6b23a685d85a2b92782
Rafa <rapala61@gmail.com>
2017-08-21 09:00:43 -0400

    deprecate twilio route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,9 @@
 const homeRoute = require('./home');
 const facebookRoute = require('./facebook');
 const slackRoute = require('./slack');
-const twilioRoute = require('./twilio');
 
 module.exports = function init(app) {
   app.get('/', homeRoute);
   app.use('/facebook/receive', facebookRoute);
   app.use('/slack/receive', slackRoute);
-  app.use('/twilio/receive', twilioRoute);
 };
