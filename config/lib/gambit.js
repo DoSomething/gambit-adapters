004d9fc0dd225f25bb93dd429661abd35cd342a0
Aaron Schachter <aaronschachter@gmail.com>
2018-12-21 20:22:35 -0800

    Cleanup

--- config/lib/gambit.js
+++ config/lib/gambit.js
@@ -6,6 +6,6 @@ module.exports = {
       name: process.env.DS_GAMBIT_CONVERSATIONS_BASIC_AUTH_NAME,
       pass: process.env.DS_GAMBIT_CONVERSATIONS_BASIC_AUTH_PASS,
     },
-    baseUri: process.env.DS_GAMBIT_CONVERSATIONS_BASEURI || 'https://gambit-conversations-staging.herokuapp.com',
+    baseUri: process.env.DS_GAMBIT_CONVERSATIONS_BASEURI || 'https://gambit-conversations-staging.herokuapp.com/api/v2',
   },
 };
