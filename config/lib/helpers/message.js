ebdf640837c7d7a7c88342a220483ff915838e4a
Aaron Schachter <aaronschachter@gmail.com>
2021-01-12 16:15:42 -0800

    Adds config for message media

---
+++ config/lib/helpers/message.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  media: {
+    url: process.env.MEDIA_URL || 'https://user-images.githubusercontent.com/1236811/104386167-6d222700-54e9-11eb-90f1-f7402020e821.JPG',
+  },
+};
