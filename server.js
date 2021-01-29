ac510129e7dc7413e644c0ba76999da41b853d8d
Aaron Schachter <aaronschachter@gmail.com>
2021-01-28 22:20:35 -0800

    WIP

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ require('dotenv').config();
 const { App } = require('@slack/bolt');
 const logger = require('heroku-logger');
 
+const { reply } = require('./lib/slack');
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 // Our Slack app is configured to listen for events that are DM messages to the bot. 
-app.message('', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered.
-  await say(`Hey there <@${message.user}>!`);
+app.message('', async ({ message, client }) => {
+  // Sends a message to the channel where the event was triggered.
+  await reply(message, client);
 });
 
 (async () => {
